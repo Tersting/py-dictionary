ee0f04b32fa2ed3bdcc8a071683ca5736accee6c
Nazar <your@mail.com>
2025-10-19 15:11:59 +0300

    SSolution2

--- app/main2.py
+++ app/main2.py
@@ -1,69 +1,57 @@
+import math
 from typing import Any
 
 
 class Dictionary:
     def __init__(self, bucket: int = 8) -> None:
+        # Number of buckets
         self.__bucket = bucket
+        # Hash table of size bucket
         self.__table = [[] for _ in range(bucket)]
         self.length = 0
 
     def __len__(self) -> int:
-        return self.length  # O(1)
+        return self.length
+
+    def resize(self) -> None:
+
+        checker_2_3 = math.floor((2 / 3) * self.__bucket)
+        if self.__len__() >= checker_2_3:
+            self.__bucket *= 2
+            copy_table = self.__table.copy()
+            self.__table = [[] for _ in range(self.__bucket)]
+
+            for slot in copy_table:
+                for lst in slot:
+                    self.__setitem__(lst[0], lst[2])
 
     def hashfunction(self, key: Any) -> tuple[int, int]:
         hash_key = hash(key)
         index = hash_key % self.__bucket
         return hash_key, index
 
-    def _insert_no_resize(self, key: Any, hash_key: int, value: Any) -> None:
-        lst = [key, hash_key, value]
-        index = hash_key % self.__bucket
-
-        if not self.__table[index]:
-            self.__table[index].append(lst)
-            self.length += 1
-            return
-
-        for i, existing in enumerate(self.__table[index]):
-            if existing[0] == key:
-                self.__table[index][i] = lst
-                return
-
-        self.__table[index].append(lst)
-        self.length += 1
-
-    def resize(self) -> None:
-        threshold = (2 * self.__bucket) // 3
-        if self.length >= threshold:
-            old_table = self.__table
-            self.__bucket *= 2
-            self.__table = [[] for _ in range(self.__bucket)]
-            self.length = 0
-
-            for slot in old_table:
-                for lst in slot:
-                    self._insert_no_resize(lst[0], lst[1], lst[2])
-
     def __setitem__(self, key: Any, value: Any) -> None:
         self.resize()
         hash_key, index = self.hashfunction(key)
+
         lst = [key, hash_key, value]
 
+        # Create
         if not self.__table[index]:
             self.__table[index].append(lst)
-            self.length += 1
             return
 
+        # Update
         for i, existing in enumerate(self.__table[index]):
             if existing[0] == key:
                 self.__table[index][i] = lst
                 return
 
         self.__table[index].append(lst)
-        self.length += 1
 
-    def __getitem__(self, key: Any) -> Any:
+    def __getitem__(self, key: Any) -> Any | None:
         hash_key, index = self.hashfunction(key)
+
         for lst in self.__table[index]:
             if lst[0] == key:
                 return lst[2]
